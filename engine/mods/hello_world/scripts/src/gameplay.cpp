db3fa15abf7bc10381c4b55318cbdefca6dba872
Alf-André Walla <fwsgonzo@hotmail.com>
2020-05-29 23:16:02 +0200

    Document most functions

--- engine/mods/hello_world/scripts/src/gameplay.cpp
+++ engine/mods/hello_world/scripts/src/gameplay.cpp
@@ -3,10 +3,15 @@
 
 __attribute__((constructor))
 static void gconstr() {
-	/* This get's called on each machine */
+	/* This gets called before anything else, on each machine */
 }
 
+/* These are used for benchmarking */
 FAST_API void empty_function() {
+	/* The ebreak-unreachable combo is a way to abruptly stop the machine, and
+	   we can also use it to make the compiler not emit function epilogues.
+	   You don't have to use this for anything other than making benchmark
+	   numbers pretty or if you have a very tight VM function call. */
 	asm("ebreak");
 	__builtin_unreachable();
 }
@@ -18,20 +23,35 @@ FAST_API void thread_function() {
 	__builtin_unreachable();
 }
 
+/* This is the function that gets called at the start */
+/* See engine/src/main.cpp:47 */
 PUBLIC_API void start()
 {
+	/* This function is implemented in api_impl.h, and it makes a
+	   system call into the engine, which then writes to the terminal. */
 	api::print("Hello world!\n");
 
+	/* This function makes thousands of calls into this machine,
+	   while preserving registers, and then prints some statistics. */
 	api::measure("VM function call overhead", empty_function);
 	api::measure("Thread creation overhead", thread_function);
 
+	/* This function tells the engine to find the "gameplay2" machine,
+	   and then make a call into it with the provided function and arguments.
+	   NOTE: We have to used the shared area to pass anything that is not
+	   passed through normal registers. See events.hpp for an example. */
 	long r = FARCALL("gameplay2", "some_function", 1234);
 	api::print("Back again in the start() function! Return value: ", r, "\n");
 
+	/* This creates a new threads with no arguments and immediately starts
+	   executing it. The sleep() will block the thread until some time has
+	   passed, and then resume. At the end we make a remote function call
+	   to a long-running process that sits in an event loop waiting for work. */
 	microthread::direct([] {
 		api::print("Hello Microthread World!\n");
 		api::sleep(1.0);
 		api::print("Hello Belated Microthread World! 1 second passed.\n");
+		/* REMOTE_EXEC is implemented in events.hpp */
 		REMOTE_EXEC("events", [] {
 			api::print("I am being run on another machine!\n");
 		});
@@ -39,6 +59,10 @@ PUBLIC_API void start()
 	api::print("Back again in the start() function!\n");
 }
 
+/* We can only call this function remotely if it's added to "gameplay.symbols",
+   because the symbol files are used in the build system to preserve certain
+   functions that would ordinarily get optimized out. It's name also has to
+   unmangled, otherwise we can't find it in the ELF string tables. */
 PUBLIC_API long some_function(int value)
 {
 	api::print("Hello Remote World! value = ", value, "!\n");
--- engine/src/main.cpp
+++ engine/src/main.cpp
@@ -7,6 +7,7 @@ static std::unique_ptr<Script> events = nullptr;
 #include "timers.hpp"
 Timers timers; // put this in a level structure
 
+/* This is the worlds fastest hash map. (C) (R) 2020 */
 Script* get_script(uint32_t machine_hash)
 {
 	if (machine_hash == gameplay1->hash())
@@ -18,12 +19,16 @@ Script* get_script(uint32_t machine_hash)
 	return nullptr;
 }
 
-#include <unistd.h> /* sleep */
+#include <unistd.h> /* usleep */
 int main()
 {
+	/* This binary will be shared among all the machines, for convenience */
 	auto binary = load_file("mods/hello_world/scripts/gameplay.elf");
 	Script::init();
+	/* Naming the machines allows us to call into one machine from another
+	   using this name (hashed). */
 	gameplay1.reset(new Script(binary, "gameplay1"));
+	/* The symbol file contains public functions that we don't want optimized away. */
 	gameplay1->hash_public_api_symbols("mods/hello_world/scripts/src/gameplay.symbols");
 
 	gameplay2.reset(new Script(binary, "gameplay2"));
@@ -32,15 +37,26 @@ int main()
 	events.reset(new Script(binary, "events"));
 	events->hash_public_api_symbols("mods/hello_world/scripts/src/gameplay.symbols");
 
+	/* The event_loop function can be resumed later, and can execute work
+	   that has been preemptively handed to it from other machines. */
 	events->call("event_loop");
 
+	/* This is the main start function, which would be something like the
+	   starting function for the current levels script. You can find the
+	   implementation in mods/hello_world/scripts/src/gameplay.cpp:28. */
 	gameplay1->call("start");
 
 	printf("...\n");
+	/* Ordinarily a game engine has a physics loop that ticks regularly,
+	   but we don't in this example. Instead we will just sleep until
+	   the next available timer. And resume the event loop in between. */
 	while (timers.active() > 0) {
 		timers.handle_events();
 		usleep(timers.next() * 1e6);
-		// this guy should probably run each engine tick
+		/* This guy should run each engine tick instead. We are passing
+		   the maximum number of instructions that we allow it to use.
+		   This can work well as a substitute for time spent, provided
+		   each complex system call increments the counter sufficiently. */
 		events->resume(5'000);
 	}
 
