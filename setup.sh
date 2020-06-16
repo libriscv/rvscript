35b209d68da447606efe59e5835763d9391838e5
Alf-André Walla <fwsgonzo@hotmail.com>
2020-06-16 12:26:36 +0200

    Fix EASTL repo setup

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+set -e
+# we have to do it this way because of EASTLs idiotic repo setup
+git submodule update --init
 pushd ext/libriscv
 git submodule update --init
 pushd lib/EASTL
