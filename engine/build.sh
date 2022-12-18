61daf0a204305550ca1113dff87369342e2750e3
Alf-André Walla <fwsgonzo@hotmail.com>
2022-12-18 12:32:50 +0100

    Build the engine and the script

--- engine/build.sh
+++ engine/build.sh
@@ -3,12 +3,19 @@ set -e
 export CXX="ccache $CXX"
 export CC="ccache $CC"
 
+# Build the game
 mkdir -p build
 pushd build
 cmake .. -DCMAKE_BUILD_TYPE=Release -DSANITIZE=OFF
-time make -j4
+make -j4
+popd
+
+# Build the script
+pushd ../programs
+source build.sh
 popd
 
 # gsettings set org.gnome.mutter center-new-windows true 
 
+# Run the game
 ./hubris
