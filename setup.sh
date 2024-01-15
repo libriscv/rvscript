69cd96fb786541229cd03b1a2284920c2a0c3e64
Alf-André Walla <fwsgonzo@hotmail.com>
2024-01-15 12:01:31 +0100

    Initialize submodules in library and use a lower version of RISC-V GNU toolchain

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 set -e
-sudo apt install ccache cmake ninja-build libglfw3-dev libglew-dev g++-12-riscv64-linux-gnu
+# We need a recent g++ version for <source_location>
+sudo apt install g++-12-riscv64-linux-gnu
+sudo apt install ccache cmake ninja-build libglfw3-dev libglew-dev
 git submodule update --init --depth 2
 git submodule update --init --recursive ext/nanogui
+git submodule update --init --recursive ext/library
 
 pushd engine
 ./build.sh
