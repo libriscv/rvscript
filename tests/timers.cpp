45f0a007df8badccdefa7338eb9e98ba557019de
Alf-André Walla <fwsgonzo@hotmail.com>
2024-01-18 16:47:46 +0100

    Add test for timer storage

--- tests/timers.cpp
+++ tests/timers.cpp
@@ -76,3 +76,54 @@ TEST_CASE("Verify periodic timer", "[Timers]")
 	REQUIRE(timers.active() == 0);
 	REQUIRE(script.call("yep_count") == 3);
 }
+
+TEST_CASE("Verify timer storage", "[Timers]")
+{
+	const auto program = build_and_load(R"M(
+	#include <api.h>
+
+	static int yep = 0;
+	extern "C" int yep_count() {
+		return yep;
+	}
+
+	int main() {
+		struct MyData {
+			int a;
+			int b;
+			int c;
+			int d;
+			int e;
+			int f;
+		} data { 1, 2, 3, 4, 5, 6 };
+
+		api::Timer::periodic(0.25f,
+		[data] (auto) {
+			assert(data.a == 1);
+			assert(data.b == 2);
+			assert(data.c == 3);
+			assert(data.d == 4);
+			assert(data.e == 5);
+			assert(data.f == 6);
+			yep += 1;
+		});
+	})M");
+
+	setup_timer_system();
+	REQUIRE(timers.active() == 0);
+
+	Script script {program, "MyScript", "/tmp/myscript"};
+
+	REQUIRE(timers.active() == 1);
+	REQUIRE(script.call("yep_count") == 0);
+
+	timers_loop([&script] {
+		REQUIRE(timers.active() == 1);
+		const int yep_count = script.call("yep_count");
+		if (yep_count >= 3)
+			timers.stop(0);
+	});
+
+	REQUIRE(timers.active() == 0);
+	REQUIRE(script.call("yep_count") == 3);
+}
